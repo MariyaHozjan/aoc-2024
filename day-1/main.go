658fa713e420a1c3958f26ce056b3f1e590692b6
Mariya <hozjan.mariya@gmail.com>
2024-12-22 22:34:50 +0100

    Solved Day 1

--- day-1/main.go
+++ day-1/main.go
@@ -1,21 +1,94 @@
 package main
 
 import (
-  "fmt"
+	"bufio"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
+func createColumns(path string) ([]int, []int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer file.Close()
+
+	var column1 []int
+	var column2 []int
+
+	//var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+
+		splt := splitString(scanner.Text())
+
+		string1, err := strconv.Atoi(splt[0])
+		if err != nil {
+			panic(err)
+		}
+		column1 = append(column1, string1)
+
+		string2, err := strconv.Atoi(splt[1])
+		if err != nil {
+			panic(err)
+		}
+		column2 = append(column2, string2)
+	}
+
+	return column1, column2, scanner.Err()
+}
+
+func calculateDifference(column1 []int, column2 []int) int {
+	sort.Ints(column2)
+	sort.Ints(column1)
+
+	Difference := 0
+
+	for i := 0; i < len(column1); i++ {
+		if column1[i] < column2[i] {
+			Difference += column2[i] - column1[i]
+			continue
+		}
+		Difference += column1[i] - column2[i]
+	}
+
+	print(Difference)
+	return Difference
+}
+
+func splitString(s string) []string {
+
+	words := strings.Fields(s)
+	return words
+}
+
+func similarityScore(column1 []int, column2 []int) int {
+	similarity := 0
+
+	for i := 0; i < len(column1); i++ {
+		similarityOfCurrentNumber := 0
+		for j := 0; j < len(column2); j++ {
+			if column1[i] == column2[j] {
+				similarityOfCurrentNumber += column1[i]
+			}
+		}
+		similarity += similarityOfCurrentNumber
+	}
+	print(similarity)
+	return similarity
+}
 
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Println("Hello and welcome, %s!", s)
+	column1, column2, err := createColumns("input.txt")
+	if err != nil {
+		panic(err)
+	}
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	print("Difference between columns: ")
+	calculateDifference(column1, column2)
+	print("\n")
+	print("Similarity score: ")
+	similarityScore(column1, column2)
 }
